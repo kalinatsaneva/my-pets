cfcb7b5f066e4a13d673101d4913a7f090e558ac
Kalina Tsaneva <k.tsaneva1@gmail.com>
2021-12-04 12:50:10 +0200

    Implemented router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import MyPets from "./components/MyPets/MyPets";
+import Create from "./components/Create/Create";
+import Edit from "./components/Edit/Edit";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="container">
+
+      <Header/>
+
+      <main id="site-content">
+    <Routes>
+    <Route path="/" element={<Dashboard />} />
+    <Route path="/login" element={<Login />} /> 
+    <Route path="/register" element={<Register />} /> 
+    <Route path="/my-pets" element={<MyPets />} /> 
+    <Route path="/create" element={<Create />} /> 
+    <Route path="/edit" element={<Edit />} /> 
+    </Routes>
+      </main>
+
+      <footer id="site-footer">
+        <p>@PetMyPet</p>
+      </footer>
+
     </div>
+
   );
 }
 
